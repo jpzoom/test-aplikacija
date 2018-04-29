8e9de17d183f5b0805675e5b8ef086fda15c4fee
Josip Panic <jolecast44@gmail.com>
2018-04-30 00:40:34 +0200

    neš besmisleno što sam stisnuo

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,9 +1,7 @@
 @extends('layouts.app')
 @section('content')
 <h1>About</h1>
-<<<<<<< HEAD
 <p>Lorem, sit amet consectetur adipisicing elit. At ullam repellendus architecto sequi delectus sint id deserunt excepturi corrupti error cum magni cupiditate nesciunt quasi, rerum dolore, eveniet repudiandae velit.</p>
 =======
 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. At ullam repellendus architecto sequi delectus sint id deserunt excepturi corrupti error cum magni cupiditate nesciunt quasi, rerum dolore, eveniet repudiandae velit.</p>
->>>>>>> master
 @endsection
